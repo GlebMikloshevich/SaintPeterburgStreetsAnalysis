5f3cae09c4bb48b5dc7763391495dfe6bf118d4f
Gleb Mikloshevich <glebmikloshevich@gmail.com>
2024-06-22 20:16:20 +0300

    feat: added detector crop before prediction

--- ImageGathering/Detector.py
+++ ImageGathering/Detector.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import numpy as np
 from collections import Counter
+from PIL import Image
 
 class Detector:
     def __init__(self, weight_path: str = "./weights/yolov8.pt"):
@@ -8,7 +9,7 @@ class Detector:
         self.target_classes = ("person", "car", "motorcycle", "bus", "truck", "boat", "dog", "horse")
         self.names = self.model.names
 
-    def predict(self, image: np.ndarray, *args, **kwargs) -> dict:
+    def predict(self, image: np.ndarray | Image.Image, *args, **kwargs) -> dict:
         labels = Counter()
         prediction = self.model.predict(image)
         bboxes = {}
@@ -25,6 +26,16 @@ class Detector:
                 bboxes[cls_label] = [self.to_int_bbox(int_bbox)]
 
         return {"labels": labels, "bboxes": bboxes}
+
+    def crop_predict(self, image: np.ndarray, crop_bbox: list[int]):
+        image_pil = Image.fromarray(image)
+        cropped_pil = image_pil.crop(crop_bbox)
+        dx, dy, _, _ = crop_bbox
+        yolo_result = self.predict(cropped_pil)
+        for label, bboxes_list in yolo_result["bboxes"].items():
+            for i, bbox in enumerate(bboxes_list):
+                bboxes_list[i] = [bbox[0] + dx, bbox[1] + dy, bbox[2] + dx, bbox[3] + dy]
+        return yolo_result, cropped_pil
 
     def __call__(self, *args, **kwargs):
         return self.predict(*args, **kwargs)
